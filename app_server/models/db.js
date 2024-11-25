5ad71758267d8f76998424b55f5d6a7a84f8391d
Annnamarie <cortes.annamarie@gmail.com>
2024-11-24 16:22:27 -0800

    Module 4 Completed Baseline

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -11,7 +11,7 @@ const connect = () => {
 
 // Monitor connection events
 mongoose.connection.on("connected", () => {
-  console.log("Mongoose connected to ${dbURI}");
+  console.log(`Mongoose connected to ${dbURI}`);
 });
 
 mongoose.connection.on("error", (err) => {
